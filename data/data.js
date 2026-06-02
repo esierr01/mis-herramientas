cb0987c5f32237dd55e54477015735a5c9f49728
Emmanuel Sierra <emmanuel.sierra@gmail.com>
2026-06-01 20:36:30 -0400

    Corrección dirección iconos

--- data/data.js
+++ data/data.js
@@ -11,37 +11,37 @@ export const shortcutSegments = [
             {
                 title: "CURSOS",
                 url: "https://www.cursosdev.com/coupons/Spanish",
-                icon: `/assets/icons/cursos.svg`
+                icon: `./assets/icons/cursos.svg`
             },
             {
                 title: "WhatsApp",
                 url: "https://web.whatsapp.com/",
-                icon: `/assets/icons/whatsapp.svg`
+                icon: `./assets/icons/whatsapp.svg`
             },
             {
                 title: "Telegram",
                 url: "https://web.telegram.org/a/",
-                icon: `/assets/icons/telegram.svg`
+                icon: `./assets/icons/telegram.svg`
             },
             {
                 title: "TikTok",
                 url: "https://www.tiktok.com/",
-                icon: `/assets/icons/tiktok.svg`
+                icon: `./assets/icons/tiktok.svg`
             },
             {
                 title: "Instagram",
                 url: "https://www.instagram.com/?hl=es",
-                icon: `/assets/icons/instagram.svg`
+                icon: `./assets/icons/instagram.svg`
             },
             {
                 title: "UNETI",
                 url: "https://www.uneti.edu.ve/campus/login/index.php",
-                icon: `/assets/icons/uneti.svg`
+                icon: `./assets/icons/uneti.svg`
             },
             {
                 title: "Trello",
                 url: "https://trello.com/u/emmanuelsierra6/boards",
-                icon: `/assets/icons/trello.svg`
+                icon: `./assets/icons/trello.svg`
             }
         ]
     },
@@ -53,32 +53,32 @@ export const shortcutSegments = [
             {
                 title: "DATATRAFIC",
                 url: "http://161.196.113.242/test_carlos/index.php",
-                icon: `/assets/icons/datatrafic.svg`
+                icon: `./assets/icons/datatrafic.svg`
             },
             {
                 title: "TalentoCANTV",
                 url: "https://talento.cantv.com.ve/login",
-                icon: `/assets/icons/talento.svg`
+                icon: `./assets/icons/talento.svg`
             },
             {
                 title: "COR",
                 url: "http://161.196.42.68/index.php",
-                icon: `/assets/icons/cor.svg`
+                icon: `./assets/icons/cor.svg`
             },
             {
                 title: "CorreoCANTV",
                 url: "https://correoweb.cantv.com.ve/",
-                icon: `/assets/icons/correoCANTV.svg`
+                icon: `./assets/icons/correoCANTV.svg`
             },
             {
                 title: "HCM-CANTV",
                 url: "https://saludenlinea.cantv.com.ve/login",
-                icon: `/assets/icons/hcm.svg`
+                icon: `./assets/icons/hcm.svg`
             },
             {
                 title: "BCV",
                 url: "https://www.bcv.org.ve/",
-                icon: `/assets/icons/bcv.svg`
+                icon: `./assets/icons/bcv.svg`
             }
         ]
     },
@@ -90,37 +90,37 @@ export const shortcutSegments = [
             {
                 title: "Gemini",
                 url: "https://gemini.google.com/app?hl=es_419",
-                icon: `/assets/icons/gemini.svg`
+                icon: `./assets/icons/gemini.svg`
             },
             {
                 title: "DeepSeek",
                 url: "https://chat.deepseek.com/",
-                icon: `/assets/icons/deepseek.svg`
+                icon: `./assets/icons/deepseek.svg`
             },
             {
                 title: "Qwen",
                 url: "https://chat.qwen.ai/",
-                icon: `/assets/icons/qwen.svg`
+                icon: `./assets/icons/qwen.svg`
             },
             {
                 title: "Z IA",
                 url: "https://chat.z.ai/",
-                icon: `/assets/icons/z.svg`
+                icon: `./assets/icons/z.svg`
             },
             {
                 title: "Kimi",
                 url: "https://www.kimi.com/",
-                icon: `/assets/icons/kimi.svg`
+                icon: `./assets/icons/kimi.svg`
             },
             {
                 title: "Lovable",
                 url: "https://lovable.dev/dashboard",
-                icon: `/assets/icons/lovable.svg`
+                icon: `./assets/icons/lovable.svg`
             },
             {
                 title: "Stich",
                 url: "https://stitch.withgoogle.com/?pli=1",
-                icon: `/assets/icons/stich.svg`
+                icon: `./assets/icons/stich.svg`
             },
         ]
     },
@@ -132,77 +132,77 @@ export const shortcutSegments = [
             {
                 title: "Netlify",
                 url: "https://app.netlify.com/teams/esierr01/projects",
-                icon: `/assets/icons/netlify.svg`
+                icon: `./assets/icons/netlify.svg`
             },
             {
                 title: "GitHub",
                 url: "https://github.com/esierr01",
-                icon: `/assets/icons/github.svg`
+                icon: `./assets/icons/github.svg`
             },
             {
                 title: "MermaidChart",
                 url: "https://mermaid.ai/live/edit#pako:eNpVjc1ugzAQhF_F2lMrkYjy6_hQqYE2l0jtIadCDlbYYJRgI2OUpsC71xBFbfe0s_PNbA8HVSAwOJ7V5SC4NmSX5pLYeckSoavW1Lzdk8XiedigIbWSeB3I-mGjSCtU01SyfLzx6wkiSb-dMCRGVPI03qxkzr9LHEiabXljVLP_6-wuaiCvWfUhbP1_R2i0qbfsyNmRLw5ck4TrGQEHSl0VwIzu0IEadc0nCf3k5mAE1pgDs2vB9SmHXI4203D5qVR9j2nVlQJs97m1qmsKbjCteKn5L4KyQJ2oThpgT_FcAayHL2AepcvIj_2IBtSNV5HvwNVeV0s38gIauh71vSAOw9GB7_mpu6RxOP4At6RzEg",
-                icon: `/assets/icons/mermaid.svg`
+                icon: `./assets/icons/mermaid.svg`
             },
             {
                 title: "ExcaliDraw",
                 url: "https://excalidraw.com/",
-                icon: `/assets/icons/excalidraw.svg`
+                icon: `./assets/icons/excalidraw.svg`
             },
             {
                 title: "DBDiagram",
                 url: "https://dbdiagram.io/d",
-                icon: `/assets/icons/dbdiagram.svg`
+                icon: `./assets/icons/dbdiagram.svg`
             },
             {
                 title: "FontAwesome",
                 url: "https://fontawesome.com/search?ic=free-collection",
-                icon: `/assets/icons/fontawesome.svg`
+                icon: `./assets/icons/fontawesome.svg`
             },
             {
                 title: "Boostrap",
                 url: "https://icons.getbootstrap.com/",
-                icon: `/assets/icons/bootstrap.svg`
+                icon: `./assets/icons/bootstrap.svg`
             },
             {
                 title: "BoxIcons",
                 url: "https://boxicons.com/icons?free=true&q=back",
-                icon: `/assets/icons/boxicons.svg`
+                icon: `./assets/icons/boxicons.svg`
             },
             {
                 title: "TechIcons",
                 url: "https://techicons.dev/?search=",
-                icon: `/assets/icons/techicons.svg`
+                icon: `./assets/icons/techicons.svg`
             },
             {
                 title: "LobeHub",
                 url: "https://lobehub.com/es/icons",
-                icon: `/assets/icons/lobehub.svg`
+                icon: `./assets/icons/lobehub.svg`
             },
             {
                 title: "Icon-icons",
                 url: "https://icon-icons.com/es/",
-                icon: `/assets/icons/icon-icons.svg`
+                icon: `./assets/icons/icon-icons.svg`
             },
             {
                 title: "w3schools",
                 url: "https://www.w3schools.com/colors/colors_picker.asp",
-                icon: `/assets/icons/w3schools.svg`
+                icon: `./assets/icons/w3schools.svg`
             },
             {
                 title: "MaterialUI",
                 url: "https://materialui.co/colors",
-                icon: `/assets/icons/materialui.svg`
+                icon: `./assets/icons/materialui.svg`
             },
             {
                 title: "Color-Hex",
                 url: "https://www.color-hex.com/",
-                icon: `/assets/icons/color-hex.svg`
+                icon: `./assets/icons/color-hex.svg`
             },
             {
                 title: "FlatUIColors",
                 url: "https://flatuicolors.com/",
-                icon: `/assets/icons/flatuicolors.svg`
+                icon: `./assets/icons/flatuicolors.svg`
             },
         ]
     },
